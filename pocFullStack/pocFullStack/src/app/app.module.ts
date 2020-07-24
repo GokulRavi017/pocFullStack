53a10e5e1cf51cf25a494d14904ca2c5e994acb8
x016523 <gokul.ravi@klm.com>
2020-07-24 14:50:56 +0200

    refactored

--- pocFullStack/pocFullStack/src/app/app.module.ts
+++ pocFullStack/pocFullStack/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    AdminComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
